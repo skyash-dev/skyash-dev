b2d009dc04994e846b3c3b10620d24cd2e3ab733
skyash101 <yashpc09@gmail.com>
2024-08-24 23:53:37 +0530

    title updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,6 @@ export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
-  
 }) {
   const currentPath = usePathname();
   return (
@@ -36,6 +35,7 @@ export default function RootLayout({
           href="https://api.fontshare.com/v2/css?f[]=satoshi@900,700,500,400&display=swap"
           rel="stylesheet"
         ></link>
+        <title>skyash</title>
       </head>
       <body className="h-full w-full bg-[#1a1818] text-[12px] md:text-[14px]">
         <Navbar currentPath={currentPath}></Navbar>
@@ -45,71 +45,69 @@ export default function RootLayout({
   );
 }
 
-function Navbar(currentPath:any){
-  return(
+function Navbar(currentPath: any) {
+  return (
     <div className="flex navbar text-white py-2 font-[50] flex-col md:flex-row items-center md:px-6">
-          <Image
-            src="/images/skyash.jpeg"
-            className="rounded-md hover:scale-110 transition-all"
-            width={48}
-            height={48}
-            alt="icon"
-          ></Image>
-          <div className="flex w-full justify-between items-center flex-col md:flex-row">
-            <div className="md:w-1/2 flex justify-evenly items-center cursor-pointer my-2">
-              <span
-                className={`${
-                  currentPath == "/" ? "bg-gray-600 text-white" : ""
-                } px-4 py-1 text-center rounded-md transition-all text-[#a2a2a2] hover:text-white border-none`}
-              >
-                <Link href="/">Home</Link>
-              </span>
-              <span
-                className={`${
-                  currentPath == "/work" ? "bg-gray-600 text-white" : ""
-                } px-4 py-1 text-center rounded-md transition-all text-[#a2a2a2] hover:text-white border-none`}
-              >
-                <Link href="/work/">Work</Link>
-              </span>
-              <span
-                className={`px-4 py-1 text-center rounded-md transition-all text-[#a2a2a2] hover:text-white border-none`}
-              >
-                <Link href="https://streams.place/skyash">Microblog</Link>
-              </span>
-              <span
-                className={`${
-                  currentPath == "/blog/photographs-0e974"
-                    ? "bg-gray-600  text-white"
-                    : ""
-                } px-4 py-1 text-center rounded-md transition-all text-[#a2a2a2] hover:text-white border-none`}
-              >
-                <Link href="/blog/photographs-0e974/">Photography</Link>
-              </span>
-            </div>
-            <div className="md:w-1/2 flex justify-evenly items-center cursor-pointer my-2">
-              <span className="text-[#a2a2a2] cursor-default">
-                elsewhere --{" "}
-              </span>
-              <span className="px-3 text-[#a2a2a2] cursor-pointer hover:">
-                <Link href="https://www.x.com/skyash101/" target="_blank">
-                  X
-                </Link>
-              </span>
-              <span className="px-3 text-[#a2a2a2] cursor-pointer hover:">
-                <Link href="https://www.github.com/skyash101/" target="_blank">
-                  GitHub
-                </Link>
-              </span>
-              <span className="px-3 text-[#a2a2a2] cursor-pointer hover:">
-                <Link
-                  href="https://www.linkedin.com/in/yash-soni-62b200237/"
-                  target="_blank"
-                >
-                  Linkedin
-                </Link>
-              </span>
-            </div>
-          </div>
+      <Image
+        src="/images/skyash.jpeg"
+        className="rounded-md hover:scale-110 transition-all"
+        width={48}
+        height={48}
+        alt="icon"
+      ></Image>
+      <div className="flex w-full justify-between items-center flex-col md:flex-row">
+        <div className="md:w-1/2 flex justify-evenly items-center cursor-pointer my-2">
+          <span
+            className={`${
+              currentPath == "/" ? "bg-gray-600 text-white" : ""
+            } px-4 py-1 text-center rounded-md transition-all text-[#a2a2a2] hover:text-white border-none`}
+          >
+            <Link href="/">Home</Link>
+          </span>
+          <span
+            className={`${
+              currentPath == "/work" ? "bg-gray-600 text-white" : ""
+            } px-4 py-1 text-center rounded-md transition-all text-[#a2a2a2] hover:text-white border-none`}
+          >
+            <Link href="/work/">Work</Link>
+          </span>
+          <span
+            className={`px-4 py-1 text-center rounded-md transition-all text-[#a2a2a2] hover:text-white border-none`}
+          >
+            <Link href="https://streams.place/skyash">Microblog</Link>
+          </span>
+          <span
+            className={`${
+              currentPath == "/blog/photographs-0e974"
+                ? "bg-gray-600  text-white"
+                : ""
+            } px-4 py-1 text-center rounded-md transition-all text-[#a2a2a2] hover:text-white border-none`}
+          >
+            <Link href="/blog/photographs-0e974/">Photography</Link>
+          </span>
         </div>
-  )
+        <div className="md:w-1/2 flex justify-evenly items-center cursor-pointer my-2">
+          <span className="text-[#a2a2a2] cursor-default">elsewhere -- </span>
+          <span className="px-3 text-[#a2a2a2] cursor-pointer hover:">
+            <Link href="https://www.x.com/skyash101/" target="_blank">
+              X
+            </Link>
+          </span>
+          <span className="px-3 text-[#a2a2a2] cursor-pointer hover:">
+            <Link href="https://www.github.com/skyash101/" target="_blank">
+              GitHub
+            </Link>
+          </span>
+          <span className="px-3 text-[#a2a2a2] cursor-pointer hover:">
+            <Link
+              href="https://www.linkedin.com/in/yash-soni-62b200237/"
+              target="_blank"
+            >
+              Linkedin
+            </Link>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
